7ab6df4dcd48410211493330145f735d2e352f86
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-12-02 23:52:19 -0600

    0.0.13

--- syncweb/__main__.py
+++ syncweb/__main__.py
@@ -14,7 +14,7 @@ from syncweb.cmds.stat import cmd_stat
 from syncweb.log_utils import log
 from syncweb.syncweb import Syncweb
 
-__version__ = "0.0.12"
+__version__ = "0.0.13"
 
 
 def cmd_version(args):
